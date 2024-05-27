f70635cdf5ef11eb97d70bdaee748dd3d4563fb1
MohammadAmin Mohammadi <amin.mohammadi@kollmorgen.com>
2024-05-27 14:06:16 +0200

    Updated info

--- WrongWindowSize/Views/MainWindow.axaml.cs
+++ WrongWindowSize/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace WrongWindowSize.Views;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        SizeLabel.Content = $"Window size: {this.ClientSize}";
     }
 }
